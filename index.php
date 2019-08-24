e8a922229176646554784eeb727cd55e8879d8b2
DongInn Kim <dikim@indiana.edu>
2019-08-24 02:16:22 -0400

    Remove New Zealand

--- index.php
+++ index.php
@@ -33,16 +33,20 @@ if(isset($_GET['action']) && $_GET['action'] == "updated"){
             <div style="background-color:white;">
               <table align="center">
                 <tr>
-                  <td><a href="action.php?country=US&typeRadios=account&tt=0" title="United States"><img src="Images/US.jpg" width=220px></a></td>
-                  <td><a href="action.php?country=UK&typeRadios=account&tt=0" title="United Kingdom"><img src="Images/UK.jpg" height=220px></a></td>
-                  <td><a href="action.php?country=AU&typeRadios=account&tt=0" title="Australia"><img src="Images/AU.jpg" width=220px></a></td>
+                  <td><a href="action.php?country=US&typeRadios=account&tt=0" title="United States"><img src="Images/US.jpg" width=270px></a></td>
+                  <td><a href="action.php?country=UK&typeRadios=account&tt=0" title="United Kingdom"><img src="Images/UK.jpg" height=270px></a></td>
+                  <td><a href="action.php?country=AU&typeRadios=account&tt=0" title="Australia"><img src="Images/AU.jpg" width=270px></a></td>
+<!--
                   <td><a href="action.php?country=NZ&typeRadios=account&tt=0" title="New Zealand"><img src="Images/NZ.jpg" height=220px></a></td>
+-->
                 </tr>
                 <tr class="country">
                     <td>United States</td>
                     <td>United Kingdom</td>
                     <td>Australia</td>
+<!--
                     <td>New Zealand</td>
+-->
                 </tr>
               </table>
             </div>
