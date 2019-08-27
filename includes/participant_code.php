32f6ea0247dec3637ed1ef32ca04566e89c959fe
DongInn Kim <dikim@indiana.edu>
2019-08-27 16:17:37 -0400

    Assign the participant ID to each country

--- includes/participant_code.php
+++ includes/participant_code.php
@@ -1,8 +1,8 @@
 <?php
 $registered_codes = array(
-  'qh78fEdsDB' => 1,
-  'KEqa7cQL2!' => 1,
-  'NhICuqrEPk' => 1,
-  'xQsxQjq7$z' => 1
+  'qh78fEdsDB' => 'US',
+  'KEqa7cQL2!' => 'GB',
+  'NhICuqrEPk' => 'AU',
+  'xQsxQjq7$z' => 'NZ'
 );
 ?>
