6b10b1b7fcf2ff0c6c34fd0ae2c25aff8159cf58
DongInn Kim <dikim@indiana.edu>
2019-08-29 17:14:11 -0400

    Redirect http to https

--- includes/header.php
+++ includes/header.php
@@ -14,6 +14,12 @@ if(!$isFirefox){
   }
   die;
 }
+if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+    $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location: ' . $location);
+    exit;
+}
 ?>
  
 <!DOCTYPE html>
