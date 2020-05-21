c88a1441a70f0aa1720c24ca36aa8bf982abdc55
Jayati <devjayati@outlook.com>
2020-05-21 15:04:51 -0400

    Update thanksMturk.php

--- PHP/thanksMturk.php
+++ PHP/thanksMturk.php
@@ -14,7 +14,7 @@ session_start();
 <div id="finalForm" style="width:60%;margin-left:auto;margin-right:auto;top:100px;font-size:125%;">
 <p>
 Thank you for participating in our study. We appreciate your time an effort. You have completed
-both our Login task and our survey. We will approve your pay of &#36;2.00 for completing both tasks shortly. We will also ensure that you will get your bonus of &#36;<?php echo $_SESSION['bonusPay'];?> as soon as we are able.
+both our Login task and our survey. We will approve your pay of &#36;2.00 for completing both tasks shortly. We will also ensure that you will get your bonus pay as soon as we are able.
     
 We’d like to tell you a little more about our study design.<b>The experiment included images of websites that we created and not actual websites.  This study was designed to investigate how different groups perceive risks and privacy in online environments. </b>  
 If you have any questions about this study, please do not hesitate to contact us: sancdas@indiana.edu.
