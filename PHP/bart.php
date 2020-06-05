e6fcb2b6911a4d708325575e33358a653488bc78
Jayati <devjayati@outlook.com>
2020-06-05 13:42:56 -0400

    inflate balloon

--- PHP/bart.php
+++ PHP/bart.php
@@ -61,7 +61,7 @@ if($_SESSION['user'] != ""){
     </head>
     <body>
         <!--<h1 style="text-align:center;">Online Ballon Analogue Risk Task (oBART)</h1>-->
-            <h1 style="text-align:center;">Please click on the balloon to inflate it. Once you are done, please click on Next Balloon. The more the balloon is inflated the larger your pay, but you receive zero for any balloon that bursts.</h1>  
+            <h1 style="text-align:center;">Please click on Inflate Balloon to inflate the balloon. Once you are done, please click on Next Balloon. The more the balloon is inflated the larger your pay, but you receive zero for any balloon that bursts.</h1>  
 				<p style="text-align:center;"> <strong> Warning: </strong> You will hear a sound on every mouse click. Please make sure to have your audio on. </p>
         <!-- load AMP here -->
         <div id="bart" style="margin: auto; text-align: center;"></div>
