a6048495d4eb43b57ebbe060d3d9d489ad210ee9
Jayati <devjayati@outlook.com>
2020-06-30 11:30:52 -0400

    Update files

--- PHP/thanksMturk.php
+++ PHP/thanksMturk.php
@@ -32,7 +32,7 @@ else {
 Thank you for participating in our study. We appreciate your time an effort. You have completed
 both our Login task and our survey. We will approve your pay of &#36;2.00 for completing both tasks shortly. We will also ensure that you will get your bonus pay as soon as we are able.
     
-We’d like to tell you a little more about our study design. <b>The experiment included images of websites that we created and not actual websites.  This study was designed to investigate how different groups perceive risks and privacy in online environments. </b>  
+We’d like to tell you a little more about our study design.<b>The experiment included images of websites that we created and not actual websites.  This study was designed to investigate how different groups perceive risks and privacy in online environments. </b>  
 If you have any questions about this study, please do not hesitate to contact us: spice@indiana.edu.
 </p>
 <!--
